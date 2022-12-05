d2e00fb38ec990878bf099416125fa1fe44fc0ff
M. David Ramiro <dramirobarragan@gmail.com>
2022-12-05 15:05:52 +0000

    end sidebar

--- admin_dashboard/lib/router/handlers/not_found.dart
+++ admin_dashboard/lib/router/handlers/not_found.dart
@@ -1,8 +1,11 @@
+import 'package:admin_dashboard/providers/menu.dart';
 import 'package:admin_dashboard/ui/views/not_found.dart';
 import 'package:fluro/fluro.dart';
+import 'package:provider/provider.dart';
 
 class NotPageFoundHandlers {
   static Handler notPageFound = Handler(handlerFunc: (context, params){
+    Provider.of<MenuProvider>(context!, listen: false).serCurrentPageUrl('/404');
     return const NotFoundPage();
   });
 }
