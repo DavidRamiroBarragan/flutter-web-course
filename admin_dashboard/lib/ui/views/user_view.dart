7718ac7fd4517353442674be39be0fc216d8010b
M. David Ramiro <dramirobarragan@gmail.com>
2022-12-09 20:15:33 +0000

    fix form

--- admin_dashboard/lib/ui/views/user_view.dart
+++ admin_dashboard/lib/ui/views/user_view.dart
@@ -2,6 +2,7 @@ import 'package:admin_dashboard/models/user.dart';
 import 'package:admin_dashboard/providers/user.dart';
 import 'package:admin_dashboard/services/notifications.dart';
 import 'package:admin_dashboard/ui/cards/white_card.dart';
+import 'package:admin_dashboard/ui/inputs/custom_inputs.dart';
 import 'package:admin_dashboard/ui/labels/custom_labels.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -47,7 +48,7 @@ class _UserViewState extends State<UserView> {
         ),
         if (user == null)
           WhiteCard(
-              child: Container(
+              child: SizedBox(
             height: 400,
             child: const CircularProgressIndicator(),
           )),
@@ -64,66 +65,127 @@ class _UserView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Table(
-        columnWidths: const {0: FixedColumnWidth(250)},
-        children: [
-          TableRow(children: [
-            WhiteCard(
-                child: Container(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text('Profile', style: CustomLabels.h2()),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      width: double.infinity,
-                      height: 160,
-                      child: Stack(
-                        children: [
-                          const ClipOval(
-                            child: Image(
-                              image: AssetImage('no-image.jpg'),
-                            ),
-                          ),
-                          Positioned(
-                            bottom: 5,
-                            right: 5,
-                            child: Container(
-                              width: 40,
-                              height: 40,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(100),
-                                  border: Border.all(
-                                      color: Colors.white, width: 5)),
-                              child: FloatingActionButton(
-                                onPressed: () {},
-                                backgroundColor: Colors.indigo,
-                                elevation: 0,
-                                child: const Icon(
-                                  Icons.camera_alt_outlined,
-                                  size: 20,
-                                ),
-                              ),
-                            ),
-                          )
-                        ],
+    return Table(
+      columnWidths: const {0: FixedColumnWidth(250)},
+      children: [
+        TableRow(children: [
+          WhiteCard(
+              child: Container(
+                width: double.infinity,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text('Profile', style: CustomLabels.h2()),
+                      const SizedBox(
+                        height: 20,
                       ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    const Text(
-                      'Nombre de usuario',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    )
-                  ]),
-            )),
-            Container()
-          ])
+                      const _Avatar(),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Text(
+                        'Nombre de usuario',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )
+                    ]),
+              )),
+          _UserForm()
+        ])
+      ],
+    );
+  }
+}
+
+class _UserForm extends StatelessWidget {
+  const _UserForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return WhiteCard(
+        title: 'Información del usuario',
+        child: Form(
+            autovalidateMode: AutovalidateMode.always,
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: CustomInputs.loginInputDecoration(
+                      hint: 'Nombre del usuario',
+                      label: 'Nombre',
+                      icon: Icons.supervised_user_circle_outlined),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: CustomInputs.loginInputDecoration(
+                      hint: 'E-mail del usuario',
+                      label: 'Correo',
+                      icon: Icons.mark_email_read_outlined),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ConstrainedBox(
+                  constraints: const BoxConstraints(maxWidth: 110),
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.indigo),
+                          shadowColor: MaterialStateProperty.all(
+                              Colors.transparent)),
+                      child: Row(
+                        children: const [
+                          Icon(Icons.save_outlined, size: 20,),
+                          Text('Guardar')
+                        ],
+                      )),
+                )
+              ],
+            )));
+  }
+}
+
+class _Avatar extends StatelessWidget {
+  const _Avatar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 150,
+      height: 160,
+      child: Stack(
+        children: [
+          const ClipOval(
+            child: Image(
+              image: AssetImage('no-image.jpg'),
+            ),
+          ),
+          Positioned(
+            bottom: 5,
+            right: 5,
+            child: Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  border: Border.all(color: Colors.white, width: 5)),
+              child: FloatingActionButton(
+                onPressed: () {},
+                backgroundColor: Colors.indigo,
+                elevation: 0,
+                child: const Icon(
+                  Icons.camera_alt_outlined,
+                  size: 20,
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
